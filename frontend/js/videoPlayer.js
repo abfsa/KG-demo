d4cf2d8b0acd8487f6ce8b83cf1827cb57f03795
EndiaEnagill <1275435540@qq.com>
2025-05-03 15:29:01 +0800

    Add files via upload

---
+++ frontend/js/videoPlayer.js
@@ -0,0 +1,35 @@
+class VideoPlayer {
+    constructor(videoId) {
+        this.video = document.getElementById(videoId);
+        this.subtitleViewer = null;
+        
+        // 绑定事件
+        this.video.addEventListener('timeupdate', () => this.onTimeUpdate());
+    }
+    
+    setSubtitleViewer(subtitleViewer) {
+        this.subtitleViewer = subtitleViewer;
+    }
+    
+    onTimeUpdate() {
+        if (this.subtitleViewer) {
+            this.subtitleViewer.update(this.video.currentTime);
+        }
+    }
+    
+    getCurrentTime() {
+        return this.video.currentTime;
+    }
+    
+    play() {
+        this.video.play();
+    }
+    
+    pause() {
+        this.video.pause();
+    }
+    
+    seekTo(time) {
+        this.video.currentTime = time;
+    }
+}
